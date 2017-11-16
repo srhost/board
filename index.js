7a16ea7470b3c6e7ee81c9b7ab3aad9af534974a
srhost <srhost@gmail.com>
2017-11-16 09:59:19 +0900

    board app added

---
+++ index.js
@@ -0,0 +1,34 @@
+// index.js
+
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var app = express();
+
+
+// DB setting
+mongoose.connect(process.env.MONGODB, { useMongoClient: true});
+var db = mongoose.connection;
+db.once('open', function() {
+    console.log('DB connected.');
+});
+db.on('error', function(err) {
+    console.log('DB ERROR : ', err);
+});
+
+// Middleware setting
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname +  '/public')); // html, css, js resource 저장 폴더
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(methodOverride('_method'));
+
+// Routes
+app.use('/', require('./routes/home'));
+
+// Port setting
+app.listen(app.get('port'), function() {
+    console.log('Express Server Running on : ', app.get('port'));
+});
